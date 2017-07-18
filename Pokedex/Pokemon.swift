2a3814bb793b23fe1e51601b8bb73f37209fb895
Sattar Hummatli <settarxan@gmail.com>
2017-07-18 13:26:17 +0400

    Created Pokemon data class for Pokemons

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Settar Hummetli on 7/18/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
