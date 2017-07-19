06133a4c67a8062c2dabb466099fe7ceed9dd3af
Sattar Hummatli <settarxan@gmail.com>
2017-07-19 14:58:34 +0400

    Renamed ViewController to MainVC

--- Pokedex/MainVC.swift
+++ Pokedex/MainVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     @IBOutlet weak var collection: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
