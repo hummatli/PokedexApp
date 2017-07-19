27cea571cac72983a835c3aaef6b1068dcbba9d2
Sattar Hummatli <settarxan@gmail.com>
2017-07-19 11:09:48 +0400

    Created IBOutlets for DetailVC

--- Pokedex/Pokemon.swift
+++ Pokedex/Pokemon.swift
@@ -11,6 +11,14 @@ import Foundation
 class Pokemon {
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvoText: String!
+    
     
     var name: String {
         return _name
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,8 +11,21 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
-    
     @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var imgMain: UIImageView!
+    @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var lblType: UILabel!
+    @IBOutlet weak var lblDefence: UILabel!
+    @IBOutlet weak var lblHeight: UILabel!
+    @IBOutlet weak var lblPokedexID: UILabel!
+    @IBOutlet weak var lblWeight: UILabel!
+    @IBOutlet weak var lblBaseAttack: UILabel!
+    @IBOutlet weak var imgCurrentEvo: UIImageView!
+    @IBOutlet weak var imgNextEvo: UIImageView!
+    @IBOutlet weak var lblEvo: UILabel!
+
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +34,8 @@ class PokemonDetailVC: UIViewController {
 
     }
 
+    @IBAction func btnBackPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
